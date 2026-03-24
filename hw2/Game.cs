67abea49b922c0848c72ca406fa4c67e87d8163d
Popovyc-h <popovichtanja86@gmail.com>
2026-03-24 14:17:35 +0200

    Add files via upload

---
+++ hw2/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string DevelopmentStudio { get; set; } = null!;
+    public string Genre { get; set; } = null!;
+    public DateTime ReleaseDate { get; set; }
+}
---
+++ hw2/GameContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1;
+
+public class GameContext : DbContext
+{
+    public DbSet<Game> Games => Set<Game>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer("Server=localhost;Database=GameDb;User Id=sa;Password=Password123$;TrustServerCertificate=True;");
+}
