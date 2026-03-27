f667643120791dc1235c21dca11df363fe633ec4
Ivan <popovichtanja86@gmail.com>
2026-03-27 16:54:35 +0200

    init

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -6,6 +6,165 @@ internal class Program
     {
         Console.WriteLine($"{g.Id} | {g.Name} | {g.DevelopmentStudio} | {g.Genre} | {g.ReleaseDate.ToString("dd.MM.yyyy")} | {g.GameMode} | {g.CopiesSold}");
     }
+
+    public static void AddGame(GameContext context)
+    {
+        Console.Write("\nEnter game name: ");
+        string name = Console.ReadLine();
+
+        bool exists = context.Games.Any(g => g.Name == name);
+
+        if (exists)
+        {
+            Console.WriteLine("A game with that name already exists");
+            return;
+        }
+
+        Console.Write("Enter development studio: ");
+        string studio = Console.ReadLine();
+
+        Console.Write("Enter genre: ");
+        string genre = Console.ReadLine();
+
+        Console.Write("Enter release date (dd.MM.yyyy): ");
+        DateTime releaseDate = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Enter game mode: ");
+        string gameMode = Console.ReadLine();
+
+        Console.Write("Enter copies sold: ");
+        int copiesSold = int.Parse(Console.ReadLine());
+
+        var newGame = new Game
+        {
+            Name = name,
+            DevelopmentStudio = studio,
+            Genre = genre,
+            ReleaseDate = releaseDate,
+            GameMode = gameMode,
+            CopiesSold = copiesSold
+        };
+
+        context.Games.Add(newGame);
+        context.SaveChanges();
+        Console.WriteLine("Game added");
+    }
+
+    public static void UpdateGame(GameContext context)
+    {
+        Console.WriteLine("Game list");
+
+        var games = context.Games.ToList();
+        foreach (var g in games)
+            PrintGame(g);
+
+        Console.Write("\nEnter the name of the game you want to update: ");
+        string name = Console.ReadLine();
+
+        bool exists = context.Games.Any(g => g.Name == name);
+
+        if (!exists)
+        {
+            Console.WriteLine("There is no game with that name");
+            return;
+        }
+        
+        var game = context.Games.FirstOrDefault(g => g.Name == name);
+
+        Console.WriteLine("What do you want to change?");
+        Console.WriteLine("1 - Name");
+        Console.WriteLine("2 - Studio");
+        Console.WriteLine("3 - Genre");
+        Console.WriteLine("4 - Date");
+        Console.WriteLine("5 - Mode");
+        Console.WriteLine("6 - Selling");
+        Console.Write("Your choice: ");
+        int choice = int.Parse(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            Console.Write("Enter new name: ");
+            string gameName = Console.ReadLine();
+
+            game.Name = gameName;
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Enter new development studio: ");
+            string studio = Console.ReadLine();
+
+            game.DevelopmentStudio = studio;
+        }
+        else if (choice == 3)
+        {
+            Console.Write("Enter new genre: ");
+            string genre = Console.ReadLine();
+
+            game.Genre = genre;
+        }
+        else if (choice == 4)
+        {
+            Console.Write("Enter new release date (dd.MM.yyyy): ");
+            DateTime releaseDate = DateTime.Parse(Console.ReadLine());
+
+            game.ReleaseDate = releaseDate;
+        }
+        else if (choice == 5)
+        {
+            Console.Write("Enter new game mode: ");
+            string gameMode = Console.ReadLine();
+
+            game.GameMode = gameMode;
+        }
+        else if (choice == 6)
+        {
+            Console.Write("Enter new copies sold: ");
+            int copiesSold = int.Parse(Console.ReadLine());
+
+            game.CopiesSold = copiesSold;
+        }
+        else
+            Console.WriteLine("Invalid choice");
+     
+        context.SaveChanges();
+        Console.WriteLine("Game updated!");
+    }
+
+    public static void DeleteGame(GameContext context)
+    {
+        Console.WriteLine("Game list");
+
+        var games = context.Games.ToList();
+        foreach (var g in games)
+            PrintGame(g);
+
+        Console.Write("\nEnter game name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter development studio: ");
+        string developmentStudio = Console.ReadLine();
+
+        var game = context.Games.FirstOrDefault(g => g.Name == name && g.DevelopmentStudio == developmentStudio);
+
+        if (game == null)
+        {
+            Console.WriteLine("Game not found");
+            return;
+        }
+
+        Console.Write("Are you sure you want to delete this game? (y/n): ");
+        string confirm = Console.ReadLine();
+
+        if (confirm == "y")
+        {
+            context.Games.Remove(game);
+            context.SaveChanges();
+            Console.WriteLine("Game deleted");
+        }
+        else
+            Console.WriteLine("Deletion cancelled");
+    }
+
     static void Main(string[] args)
     {
         using var context = new GameContext();
@@ -122,10 +281,14 @@ internal class Program
         var top3BestSelling = context.Games.OrderByDescending(g => g.CopiesSold).Take(3).ToList();
         foreach (var t in top3BestSelling)
             PrintGame(t);
-        
+
         Console.WriteLine();
         var top3WorstSelling = context.Games.OrderBy(g => g.CopiesSold).Take(3).ToList();
         foreach (var t in top3WorstSelling)
             PrintGame(t);
+
+        AddGame(context);
+        UpdateGame(context);
+        DeleteGame(context);
     }
 }
