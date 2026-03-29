2c230547c94029167fcca578ff254bd97848b938
Ivan <popovichtanja86@gmail.com>
2026-03-29 14:01:50 +0300

    init

---
+++ hw3/ConsoleApp1/Migrations/20260329102206_AddCourseStatsView.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCourseStatsView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE VIEW CourseStats AS " +
+                "SELECT C.Title, COUNT(Id) AS StudentCount " +
+                "FROM Student AS S " +
+                "JOIN CourseStudent AS CS ON S.Id = CS.StudentsId " +
+                "JOIN Course AS C ON C.Id = CS.CoursesId " +
+                "GROUP BY C.Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW CourseStats");
+        }
+    }
+}
--- hw3/ConsoleApp1/Program.cs
+++ hw3/ConsoleApp1/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace ConsoleApp1;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1;
 
 internal class Program
 {
+    public static async Task<List<Instructor>> GetFullAcademyDataAsync(AcademyDbContext db)
+    {
+        // ВАШ КОД ТУТ (Крок 1):
+        // Напишіть запит, який дістає всіх викладачів.
+        // Використайте .Include() щоб підтягнути їхні Courses.
+        // Використайте .ThenInclude() щоб для кожного курсу підтягнути CourseDetail.
+        // Використайте ще один .Include() або .ThenInclude() для підтягування Students на курсах.
+
+        return await db.Instructors
+            .Include(I => I.Courses)
+            .ThenInclude(c => c.CourseDetail)
+            .Include(i => i.Courses)
+            .ThenInclude(c => c.Students)
+            .ToListAsync();
+}
+
     static void Main(string[] args)
     {
 
